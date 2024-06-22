40e17b8c0be9c982d5a5c10b88eb2830e45feccc
aravindhkumaran <aravindh1097@gmail.com>
2024-06-22 01:02:37 -0400

    commiting the chnages specific to typing library, updated | symbol with union as python 3.9 doesnt allow

--- source/shapes.py
+++ source/shapes.py
@@ -13,28 +13,28 @@ class Shape:
 
 class Circle(Shape):
 
-    def __init__(self, radius: int | float):
+    def __init__(self, radius: Union[int, float]):
         self.radius = radius 
 
-    def area(self) -> int | float:
+    def area(self) -> Union[int, float]:
         return math.pi * self.radius ** 2
     
 
-    def perimeter(self) -> int | float:
+    def perimeter(self) -> Union[int, float]:
         return 2 * math.pi * self.radius 
 
 
 class Rectangle:
 
-    def __init__(self, length: int | float, width: int | float):
+    def __init__(self, length: Union[int, float], width: Union[int, float]):
         self.length = length 
         self.width = width 
 
 
-    def area(self) -> int | float:
+    def area(self) -> Union[int, float]:
         return self.length * self.width
 
-    def perimeter(self) -> int | float:
+    def perimeter(self) -> Union[int, float]:
         return (self.length * 2) + (self.width * 2)
 
 class Square(Rectangle):
